17b6167eec4b3380c5e550f477a711b35f6809d4
雪小花 <lijiaxin@mtfxh.wiki>
2023-07-07 22:40:33 +0800

    提交格式化

--- client/web/src/@types/AxiosType.d.ts
+++
@@ -1,5 +0,0 @@
-// 请求拦截器配置定义接口
-declare interface RequestConfig {
-  baseURL: string,
-  headers: any
-}
--- client/web/src/request/index.ts
+++ client/web/src/request/index.ts
@@ -1,16 +1,16 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
 const request = axios.create({
   timeout: 3600000
 })
 // @ts-ignore
-request.interceptors.request.use((config : RequestConfig<string>) => {
-  // @ts-ignore
+request.interceptors.request.use((config : AxiosRequestConfig) => {
+  // @ts-ignore/**/
   config.baseURL = (import.meta.env.VITE_APP_REQUEST_BASE || '')
   return config
 })
-request.interceptors.response.use(config => {
-  return config
-}, (error) => {
+request.interceptors.response.use((res : AxiosResponse) => {
+  return res
+}, (error: AxiosError) => {
   return error
 })
 export default request
